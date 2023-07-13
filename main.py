cc149c49f6a7a4927e70e309b3c8b5722ff842c3
Eslam Mostafa <ee2010011@gmail.com>
2023-07-13 17:21:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,41 +1,20 @@
-import asyncio
 import os
-import subprocess
-
-import jwt
-import yaml
-from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 import uvicorn as uvicorn
-from fastapi import FastAPI, Request, WebSocket, Depends, HTTPException
-from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from fastapi import FastAPI
 
 from app.api.v1.APIv1 import APIv1
-
-file_path_session = os.path.join('data', 'apscheduler.json')
-secret_key = os.getenv("SECRET_KEY")
 
 app = FastAPI()
 app.include_router(APIv1())
 
 
-async def test():
-    code = '''
-    def print_hi(name):
-        print(f'Hi, {name}')  
-
-
-    if __name__ == '__main__':
-        print_hi('PyCharm')
-
-        '''
-
-
-    with open("secret_key.py", "wb") as f:
-        f.write(code)
+def create_folder(folder_name):
+    if not os.path.exists(folder_name):
+        os.mkdir(folder_name)
 
 
 if __name__ == "__main__":
+    create_folder("tools_v1")
+    create_folder("database")
     uvicorn.run(app, host="0.0.0.0", port=8001)
-
